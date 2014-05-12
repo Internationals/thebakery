12dc07e484c6639829990931aa0253f59714d4cb
Student Account <user22@CST1Training.(none)>
2014-05-12 19:40:28 -0400

    Start of our home page

---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head> </head>
+		<meta charset='utf-8'>
+	<title>International Bakery</title>
+
+
+
+
+
+
+<body>
+
+<div id="menu"> 
+	<ul>
+		<li><a href="home.php">Home</a></li>
+		<li><a href="about.php">About</a></li>
+		<li><a href="login.php">Login</a></li>
+	</ul>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html> 
